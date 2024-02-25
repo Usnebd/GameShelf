15c474f74f0c64198a487c4a94e55a01c66282c7
Usnebd <37882253+Usnebd@users.noreply.github.com>
2024-02-25 19:11:38 +0100

    fixed useEffect onAuthStateChanged, observer intalled only when component is mounted

--- src/App.tsx
+++ src/App.tsx
@@ -192,7 +192,7 @@ function App() {
       }
     });
     return () => unsubscribe();
-  }, [auth]);
+  }, []);
 
   useEffect(() => {
     const q = query(collection(db, `users/${user?.email}/cart`));
